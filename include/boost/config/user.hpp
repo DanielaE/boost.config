bf0544f4b2612181cef123a225ba2a65be09fdde
Daniela Engert <dani@ngrt.de>
2015-04-19 10:46:00 +0200

    Always include Boost version.
    Signed-off-by: Daniela Engert <dani@ngrt.de>
    (cherry picked from commit 06a537170f78bd1bf98d6b6ba14dd1aad9b9ce9e)
    
    # Conflicts:
    #	include/boost/config/user.hpp

--- include/boost/config/user.hpp
+++ include/boost/config/user.hpp
@@ -131,6 +131,7 @@
 // to ensure the correct libraries are selected at link time.
 // #define BOOST_LIB_BUILDID amd64
 
+#include <boost/version.hpp>
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0601
 #endif
